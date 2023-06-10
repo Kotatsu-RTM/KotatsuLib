f41d8a6f2284877e94921971a4a1dcdde8078727
Siro_256 <siro@siro256.dev>
2023-06-10 22:54:28 +0900

    Setup Forge for mc1_12_2

--- mc1_12_2/build.gradle.kts
+++ mc1_12_2/build.gradle.kts
@@ -1,7 +1,25 @@
+import net.minecraftforge.gradle.userdev.UserDevExtension
+
 group = rootProject.group.toString() + ".core"
+
+buildscript {
+    dependencies {
+        classpath("net.minecraftforge.gradle:ForgeGradle:5.1.69") {
+            isChanging = true
+        }
+    }
+}
+
+apply(plugin = "net.minecraftforge.gradle")
 
 dependencies {
     api(project(":api"))
+
+    add("minecraft", "net.minecraftforge:forge:1.12.2-14.23.5.2860")
+}
+
+configure<UserDevExtension> {
+    mappings("snapshot", "20171003-1.12")
 }
 
 tasks {
