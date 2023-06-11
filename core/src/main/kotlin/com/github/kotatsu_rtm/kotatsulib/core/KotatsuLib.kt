8a2a744acdae0012be2bc3883b8337f1f889d489
Siro_256 <siro@siro256.dev>
2023-06-12 02:36:21 +0900

    Create constants holder class

---
+++ core/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/core/KotatsuLib.kt
@@ -0,0 +1,7 @@
+package com.github.kotatsu_rtm.kotatsulib.core
+
+object KotatsuLib {
+    const val MOD_ID = "@modId@"
+    const val MOD_NAME = "@modName@"
+    const val MOD_VERSION = "@modVersion@"
+}
