99b705c351fdb2eca282f9af54d322fbe4dcf210
Siro_256 <siro@siro256.dev>
2023-06-12 14:31:55 +0900

    Create enums of GL_DEBUG_* constants

---
+++ api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/enum/gl/DebugSeverity.kt
@@ -0,0 +1,10 @@
+package com.github.kotatsu_rtm.kotatsulib.api.enum.gl
+
+import org.lwjgl.opengl.GL43
+
+enum class DebugSeverity(val constant: Int) {
+    GL_DEBUG_SEVERITY_HIGH(GL43.GL_DEBUG_SEVERITY_HIGH),
+    GL_DEBUG_SEVERITY_MEDIUM(GL43.GL_DEBUG_SEVERITY_MEDIUM),
+    GL_DEBUG_SEVERITY_LOW(GL43.GL_DEBUG_SEVERITY_LOW),
+    GL_DEBUG_SEVERITY_NOTIFICATION(GL43.GL_DEBUG_SEVERITY_NOTIFICATION)
+}
