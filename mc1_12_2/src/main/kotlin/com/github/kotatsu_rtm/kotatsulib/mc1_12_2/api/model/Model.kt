a4828ddbf5667fcdd7b649e3c83000033ea4c7aa
Siro_256 <siro@siro256.dev>
2023-06-14 18:36:06 +0900

    Create buffer class for Minecraft 1.12.2, to make it easy to use

---
+++ mc1_12_2/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/mc1_12_2/api/model/Model.kt
@@ -0,0 +1,27 @@
+package com.github.kotatsu_rtm.kotatsulib.mc1_12_2.api.model
+
+import com.github.kotatsu_rtm.kotatsulib.api.model.ModelApi
+import com.github.kotatsu_rtm.kotatsulib.api.model.native.NativeFaceWrapper
+import dev.siro256.forgelib.rtm_glsl.format.VertexFormat
+import jp.ngt.ngtlib.renderer.model.GroupObject
+
+abstract class Model(groupObjects: List<GroupObject>) : ModelApi(transformGroupObjects(groupObjects)) {
+    companion object {
+        private fun transformGroupObjects(groupObjects: List<GroupObject>) =
+            groupObjects.associate { groupObject ->
+                groupObject.name to groupObject.faces.map {
+                    fun getVertexNormalUV(index: Int) =
+                        VertexFormat.VertexNormalUV(
+                            it.vertices[index].x, it.vertices[index].y, it.vertices[index].z,
+                            it.vertexNormals[index].x, it.vertexNormals[index].y, it.vertexNormals[index].z,
+                            it.textureCoordinates[index].u, it.textureCoordinates[index].v
+                        )
+
+                    NativeFaceWrapper(
+                        it.materialId.toInt(),
+                        Triple(getVertexNormalUV(0), getVertexNormalUV(1), getVertexNormalUV(2))
+                    )
+                }
+            }
+    }
+}
