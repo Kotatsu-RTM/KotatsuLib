80bbf6c3a71ccde90f9b17167c428808cbdc7e88
Siro_256 <siro@siro256.dev>
2023-06-14 18:32:33 +0900

    Create DrawGroup

---
+++ api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/model/DrawGroup.kt
@@ -0,0 +1,29 @@
+package com.github.kotatsu_rtm.kotatsulib.api.model
+
+import dev.siro256.forgelib.rtm_glsl.enum.GLBufferUsage
+import dev.siro256.forgelib.rtm_glsl.wrapper.IndexBufferObject
+import java.util.*
+
+class DrawGroup(materialIndicesPair: MaterialIndicesPair) {
+    private val iboInfos =
+        materialIndicesPair
+            .filterValues { it.isNotEmpty() }
+            .let { pair ->
+                var offset = 0
+                pair.map {
+                    val size = it.value.size * 3
+                    val iboInfo = IboInfo(offset, size)
+                    offset += size
+
+                    it.key to iboInfo
+                }
+            }
+
+    val ibo =
+        IndexBufferObject(
+            GLBufferUsage.GL_STATIC_DRAW,
+            materialIndicesPair.flatMap { it.value }.flatMap { it.toList() }.toIntArray()
+        )
+
+    fun getIndices(materialId: Int) = Optional.ofNullable(iboInfos[materialId])
+}
