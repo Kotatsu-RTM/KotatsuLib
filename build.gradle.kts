613193359ae7f900d59f090a6e7374af7fba11db
Siro_256 <siro@siro256.dev>
2023-06-15 23:10:36 +0900

    Make project publishable

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.21"
+    `maven-publish`
+    signing
 }
 
 group = "com.github.kotatsu-rtm.kotatsulib"
@@ -20,6 +22,8 @@ allprojects {
     }
 
     apply(plugin = "kotlin")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     version = rootProject.version
 
@@ -79,5 +83,85 @@ allprojects {
 
             dependsOn("includeReadmeAndLicense")
         }
+
+        create<Jar>("sourcesJar") {
+            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+            destinationDirectory.set(rootProject.libsDirectory)
+
+            archiveBaseName.set(rootProject.name)
+            archiveAppendix.set(project.name)
+            archiveVersion.set(rootProject.version.toString())
+            archiveClassifier.set("sources")
+            archiveExtension.set("jar")
+
+            from(sourceSets.main.get().allSource)
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("publication") {
+                groupId = rootProject.group.toString()
+                artifactId = "${rootProject.name}-${project.name}"
+                version = rootProject.version.toString()
+
+                from(components.getByName("java"))
+                artifact(tasks.getByName("sourcesJar"))
+
+                pom {
+                    name.set(artifactId)
+                    description.set(rootProject.description)
+                    url.set("https://github.com/Kotatsu-RTM/KotatsuLib")
+
+                    licenses {
+                        license {
+                            name.set("The MIT License")
+                            url.set("https://opensource.org/licenses/mit-license.php")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("Siro256")
+                            name.set("Siro_256")
+                            email.set("siro@siro256.dev")
+                            url.set("https://github.com/Siro256")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/Kotatsu-RTM/KotatsuLib.git")
+                        developerConnection.set("scm:git:ssh://github.com/Kotatsu-RTM/KotatsuLib.git")
+                        url.set("https://github.com/Kotatsu-RTM/KotatsuLib.git")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                url =
+                    if (version.toString().endsWith("SNAPSHOT")) {
+                        uri("https://repo.siro256.dev/repository/maven-snapshots")
+                    } else {
+                        uri("https://repo.siro256.dev/repository/maven-public")
+                    }
+
+                credentials {
+                    username = System.getenv("NexusUsername")
+                    password = System.getenv("NexusPassword")
+                }
+            }
+        }
+    }
+
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("SigningKeyId"),
+            System.getenv("SigningKey"),
+            System.getenv("SigningKeyPassword")
+        )
+        sign(publishing.publications.getByName("publication"))
     }
 }
