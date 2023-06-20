1cf18df448937f0fef0a8f0fb1031f9ccb482c77
Siro_256 <siro@siro256.dev>
2023-06-21 00:11:30 +0900

    Suppress unsed for TexturedWithColorShader.kt

--- api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/shader/TexturedWithColorShader.kt
+++ api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/shader/TexturedWithColorShader.kt
@@ -14,6 +14,7 @@ import org.lwjgl.opengl.GL43
 import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
+@Suppress("unused")
 object TexturedWithColorShader : Shader<TexturedWithColorShader.RenderData>(
     TexturedWithColorShader::class.java.classLoader
         .getResourceAsStream("shader/textured_with_color.vsh")!!
