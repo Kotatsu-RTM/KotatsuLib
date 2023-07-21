53f346c6a6c39f786fe34ae7e37fa91b396994f1
Siro_256 <siro@siro256.dev>
2023-07-21 12:02:29 +0900

    Change function name

--- api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/gl/GLState.kt
+++ api/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/api/gl/GLState.kt
@@ -3,7 +3,7 @@ package com.github.kotatsu_rtm.kotatsulib.api.gl
 import org.joml.Matrix4f
 
 interface GLState {
-    fun getModelView(): Matrix4f
+    fun getView(): Matrix4f
 
     fun getProjection(): Matrix4f
 }
--- mc1_12_2/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/mc1_12_2/api/gl/GLStateImpl.kt
+++ mc1_12_2/src/main/kotlin/com/github/kotatsu_rtm/kotatsulib/mc1_12_2/api/gl/GLStateImpl.kt
@@ -9,21 +9,24 @@ import org.lwjgl.opengl.GL11
 import kotlin.properties.Delegates
 
 object GLStateImpl : GLState {
+    private var viewMatrix by Delegates.notNull<Matrix4f>()
     private var projectionMatrix by Delegates.notNull<Matrix4f>()
     private val matrixBuffer = GLAllocation.createDirectFloatBuffer(16)
 
-    override fun getModelView() =
-        matrixBuffer.apply {
-            rewind()
-            GL11.glGetFloat(GL11.GL_MODELVIEW_MATRIX, this)
-            rewind()
-        }.let { Matrix4f(it) }
+    override fun getView() = viewMatrix
 
     override fun getProjection() = projectionMatrix
 
     @SubscribeEvent
     fun onStartRender(event: TickEvent.RenderTickEvent) {
         if (event.phase != TickEvent.Phase.START) return
+
+        viewMatrix =
+            matrixBuffer.apply {
+                rewind()
+                GL11.glGetFloat(GL11.GL_MODELVIEW_MATRIX, this)
+                rewind()
+            }.let { Matrix4f(it) }
 
         projectionMatrix =
             matrixBuffer.apply {
